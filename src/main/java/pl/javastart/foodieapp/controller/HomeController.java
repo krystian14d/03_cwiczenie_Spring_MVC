bccefc6351c61964ccd59b10dd0eaf290515c940
Krystian <krystian14d@gmail.com>
2022-03-04 14:17:27 +0100

    update

--- src/main/java/pl/javastart/foodieapp/controller/HomeController.java
+++ src/main/java/pl/javastart/foodieapp/controller/HomeController.java
@@ -1,23 +1,26 @@
-package pl.javastart.foodieapp;
+package pl.javastart.foodieapp.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.javastart.foodieapp.item.Item;
 import pl.javastart.foodieapp.item.ItemRepository;
 
 import java.util.List;
 
 @Controller
+@ComponentScan
 public class HomeController {
 
-    private final ItemRepository itemRepository;
+    private ItemRepository itemRepository;
 
     public HomeController(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String home(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
