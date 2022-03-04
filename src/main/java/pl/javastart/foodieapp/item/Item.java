2b878422e5bff5b20604181dc01ab9fb080e0844
Krystian <krystian14d@gmail.com>
2022-03-04 09:38:28 +0100

    files added

--- src/main/java/pl/javastart/foodieapp/item/Item.java
+++ src/main/java/pl/javastart/foodieapp/item/Item.java
@@ -1,55 +1,25 @@
 package pl.javastart.foodieapp.item;
 
-import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
 @Entity
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
     private Long id;
     private Double price;
     private String name;
     private String shortDescription;
     @Column(length = 1024)
     private String description;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/main/java/pl/javastart/foodieapp/order/Order.java
+++ src/main/java/pl/javastart/foodieapp/order/Order.java
@@ -1,26 +1,38 @@
 package pl.javastart.foodieapp.order;
 
+import lombok.Getter;
+import lombok.Setter;
 import pl.javastart.foodieapp.item.Item;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "client_order")
 public class Order {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToMany
     @JoinTable(name = "order_item",
-    joinColumns = @JoinColumn(name = "order_id", referencedColumnName = "id"),
-    inverseJoinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id"))
+            joinColumns = @JoinColumn(name = "order_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id", referencedColumnName = "id"))
     private List<Item> items = new ArrayList<>();
     private String address;
     private String telephone;
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
-
-
 }
