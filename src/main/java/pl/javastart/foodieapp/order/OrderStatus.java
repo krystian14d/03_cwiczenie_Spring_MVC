81e70a0ad3601f02d662dea2cd7f9e937e2ad87a
Krystian <krystian14d@gmail.com>
2022-03-05 18:15:03 +0100

    added panel to manage orders

--- src/main/java/pl/javastart/foodieapp/order/OrderStatus.java
+++ src/main/java/pl/javastart/foodieapp/order/OrderStatus.java
@@ -4,4 +4,13 @@ public enum OrderStatus {
     NEW,
     IN_PROGRESS,
     COMPLETE;
+
+    static OrderStatus nextStatus(OrderStatus status) {
+        if (status == NEW)
+            return IN_PROGRESS;
+        else if (status == IN_PROGRESS)
+            return COMPLETE;
+        else
+            throw new IllegalArgumentException("There is no next status for provided value.");
+    }
 }
